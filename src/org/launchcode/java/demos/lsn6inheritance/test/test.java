b26c42b90296ceb68a5f0d945d25e90dc4c3c289
nbnhunt <nbnhunt@outlook.com>
2022-09-27 13:18:09 -0500

    Part of chapter 7

---
+++ src/org/launchcode/java/demos/lsn6inheritance/test/test.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.demos.lsn6inheritance.test;
+
+public class test {
+
+    public static void test() {
+        @Test
+                putlic void inheritsSuperInFirstConstructor() {
+            HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+            assertEquals(7, keyboardCat.getWeight(), .001);
+        }
+    }
+}
